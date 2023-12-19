a923108ec2a00a784fc9e30d0034d95f0f1a2e5a
faten29102021 <154262361+faten29102021@users.noreply.github.com>
2023-12-19 11:59:11 +0200

    add3 (#3)
    Co-authored-by: faten <faten@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int sum2(int a ,int b)
->>>>>>> math_feature
+
+int sum3(int a ,int b)
+
+
 int main(){
     cout<<"dear student";
     cout<<"let's try new concepts";
     cout<<"learning git is fun";
 }
 
-int sum2(int a,int b){
+
+int sum3(int a,int b){
+
     return a+b;
->>>>>>> math_feature
+
 }
