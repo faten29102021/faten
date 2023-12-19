938b335a48b6de2323ea7662adb07641348cd587
faten <faten@gmail.com>
2023-12-19 10:46:19 +0200

    base commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int my_sum(int a ,int b)
+int main(){
+    cout<<"dear student";
+    cout<<"let's try new concepts";
+    cout<<"learning git is fun";
+}
+int my_sum(int a,int b){
+    return a+b;
+}
