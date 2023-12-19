0b7d6642a259606fc8f76877e1d69e29e8e733fd
faten <faten@gmail.com>
2023-12-19 10:54:22 +0200

    math commit last

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-int my_sum(int a ,int b)
+int sum(int a ,int b)
 int main(){
     cout<<"dear student";
     cout<<"let's try new concepts";
     cout<<"learning git is fun";
 }
-int my_sum(int a,int b){
+int sum(int a,int b){
     return a+b;
 }
